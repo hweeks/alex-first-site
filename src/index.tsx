a25f4406a7345aa132fc7178d41944c6eca076ea
asorichetti <107128425+asorichetti@users.noreply.github.com>
2024-05-28 16:55:07 -0400

    Made it so there is a background color todo: Make background color apply to entire page not just lines with content

--- src/index.tsx
+++ src/index.tsx
@@ -1,26 +1,37 @@
-import React from "react";
+import * as React from "react";
 import ReactDOM from "react-dom/client";
 import styled from "styled-components";
 import { createGlobalStyle } from 'styled-components';
 import { reset } from 'styled-reset';
 
 
+const MainStyle = createGlobalStyle`
+  ${reset}
 
-const StyleType = createGlobalStyle`
-${reset}
-
-html {
-  box-sizing: border-box;
-  font-family: 'Montserrat', regular;
-}
-*{
-  box-sizing: inherit;
-}
+  html {
+    box-sizing: border-box;
+    font-family: 'Covered By Your Grace', cursive;
+  }
+  *, *:before, *:after {
+    box-sizing: inherit;
+  }
 `;
+
+const styles = {
+  container: {
+    textAlign: 'center',
+    margin: 0,
+    padding: '20px',
+    backgroundColor: '#334445',
+    color: '#F5D547',
+  },
+};
 const HomePage = () => {
 
 return( 
-  "no"
+  <div style={styles.container}>
+  <p>No</p>
+  </div>
 )
 
 }
